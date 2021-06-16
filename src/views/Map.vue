6af1a17fd652d704d0d322ceaa6a7b40e86ff5f5
ryan <ball.ryan.rb@gmail.com>
2021-06-16 13:35:14 -0600

    adds Mapbox map

---
+++ src/views/Map.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<style>
+#map {
+  width: 100%;
+  height: 650px;
+}
+</style>
+
+<script>
+/* global mapboxgl */
+export default {
+  data: function () {
+    return {};
+  },
+  created: function () {},
+  mounted: function () {
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_ACCESS_TOKEN;
+    new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [1.5209, 41.5912], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  },
+  methods: {},
+};
+</script>
