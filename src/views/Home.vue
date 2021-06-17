44750176cc74e61aac98810de251a3c2f5124944
ryan <ball.ryan.rb@gmail.com>
2021-06-17 13:33:19 -0600

    adds SweetAlerts2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>
+      <button @click="showAlert()">Hello world</button>
+    </div>
   </div>
 </template>
 
@@ -9,10 +12,22 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 
+/* global Swal */
 export default {
   name: "Home",
   components: {
     HelloWorld,
+  },
+  created: function () {},
+  methods: {
+    showAlert() {
+      Swal.fire({
+        title: "Hello!",
+        text: "Hello from SweetAlerts!",
+        icon: "success",
+        confirmButtonText: "Rad",
+      });
+    },
   },
 };
 </script>
